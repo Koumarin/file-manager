5b503494483fedecc141d562074f0a114f5d78a9
Alice Figueiredo <alicefigueiredo837@gmail.com>
2023-02-24 01:08:09 -0300

    change window title on directory change

---
+++ lib/util.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var directory := Directory.new()
+
+func get_subdirectories(path: String) -> Array:
+	var lst  := []
+	var dir  := Directory.new()
+	var file := ''
+	
+	dir.open(path)
+	dir.list_dir_begin(true, true)
+	file = dir.get_next()
+	while '' != file:
+		if dir.current_is_dir():
+			lst.append(file)
+		file = dir.get_next()
+	return lst
+
+func translate_path_name(path: String) -> String:
+	var username := get_username()
+	var home     := '/home/' + username
+	match path:
+		'/':  return 'Root'
+		home: return 'Home'
+		_:    return get_relative_path(path)
+
+func get_relative_path(path: String) -> String:
+	path = path.get_basename().trim_suffix('/')
+	return path.substr(path.rfind('/') + 1)
+
+func get_username() -> String:
+	if OS.has_environment('USER'):
+		return OS.get_environment('USER')
+	else:
+		return ''
